52cb1e9e2017ac56673986772040ea166549b5c8
Michael Brel <brel.mihail@mail.ru>
2019-10-10 10:08:06 +0300

    Add SsdPage

---
+++ sandisk/pages/SsdPage.js
@@ -0,0 +1,19 @@
+const format = require('string-format');
+const Actions = require('../util/Actions');
+let actions = new Actions();
+
+
+class SsdPage {
+    
+    constructor() {
+        this.learnMoreXpath = "//*[contains(text(),'{}')]/following-sibling::*//a[contains(text(),'Learn More')]"
+    }
+
+    clickLearnMoreButton(productTitle) {
+        let learnMoreButton = element(by.xpath(format(this.learnMoreXpath, productTitle)));
+        return actions.moveAndClick(learnMoreButton);
+    }
+
+}
+
+module.exports = SsdPage;
