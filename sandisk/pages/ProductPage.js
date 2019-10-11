e1aba42ef9b7084097ce113dd321a979dfd8184a
Michael Brel <brel.mihail@mail.ru>
2019-10-11 09:55:28 +0300

    Add methods to ProductPage

--- sandisk/pages/ProductPage.js
+++ sandisk/pages/ProductPage.js
@@ -1,12 +1,28 @@
+const format = require('string-format');
+const Actions = require('../util/Actions');
+let actions = new Actions();
+
 class ProductPage {
-    
+
     constructor() {
         this.productTitle = element(by.css(".sixty.text-left.chooser h2"));
+        this.productCapacity = element(by.xpath("//div[@style='display: block;']//li//strong[contains(text(), 'Capacity:')]/parent::*"));
+        this.productCapacityButtonXpath = "//div[@class='clearer']//*[contains(text(),'{}')]";
     }
 
     getProductTitle() {
         return this.productTitle.getText();
     }
+
+    chooseProductCapacity(capacity) {
+        let productCapacityButton = element(by.xpath(format(this.productCapacityButtonXpath, capacity)));
+        return actions.moveAndClick(productCapacityButton);
+    }
+
+    getProductCapacity() {
+        return this.productCapacity.getText();
+    }
+
 }
 
 module.exports = ProductPage;
--- sandisk/pages/SsdPage.js
+++ sandisk/pages/SsdPage.js
@@ -2,11 +2,10 @@ const format = require('string-format');
 const Actions = require('../util/Actions');
 let actions = new Actions();
 
-
 class SsdPage {
     
     constructor() {
-        this.learnMoreXpath = "//*[contains(text(),'{}')]/following-sibling::*//a[contains(text(),'Learn More')]"
+        this.learnMoreXpath = "//*[contains(text(),'{}')]/following-sibling::*//a[contains(text(),'Learn More')]";
     }
 
     clickLearnMoreButton(productTitle) {
